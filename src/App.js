2cdbb0a12220a420740356ef6ff11174ae354777
M4R1US <baltr.marius@gmail.com>
2020-05-01 20:31:20 +0200

    Homepage component together with styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import HomePage from "./components/homepage.component";
 
 function App() {
   return (
-    <div>Begin</div>
+    <div>
+      <HomePage />
+    </div>
   );
 }
 
