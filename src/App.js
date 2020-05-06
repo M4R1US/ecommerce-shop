8bfda65748a79aef1756e07b221f8b6bb2bc81d3
M4R1US <baltr.marius@gmail.com>
2020-05-06 20:47:12 +0200

    Introduce user.reducer & user.actions, auto-redirect to index once user is logged in~

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom'
+import {Switch, Route, Redirect} from 'react-router-dom'
 import { connect } from "react-redux"
 
 import './App.css';
@@ -10,6 +10,7 @@ import Header from "./components/header/header.component";
 import SignInSignUpPage from "./pages/sign-in-and-sign-up/sing-in-sign-up.component";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { setCurrentUser } from "./redux/user/user.actions";
+
 
 class App extends React.Component{
 
@@ -43,17 +44,19 @@ class App extends React.Component{
         <Switch>
           <Route exact path="/" component={HomePage}/>
           <Route exact path="/shop" component={ShopPage}/>
-          <Route exact path="/signin" component={SignInSignUpPage}/>
+          <Route exact path="/signin" render= {() => this.props.currentUser ? (<Redirect to="/" />) : (<SignInSignUpPage />)} />
         </Switch>
       </div>
     );
   }
-
-
 }
+
+const mapStateToProps = ({ user }) =>({
+  currentUser: user.currentUser
+})
 
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
