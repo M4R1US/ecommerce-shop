40a0cfc8d71155be9405acb5d78fc2a108c514db
M4R1US <baltr.marius@gmail.com>
2020-05-08 20:29:41 +0200

    Add cart reducer functionality with utils, change footer

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,5 +1,10 @@
-import { CartActionTypes } from "./cart.types";
+import CartActionTypes from './cart.types';
 
 export const toggleCart = () => ({
   type: CartActionTypes.TOGGLE_CART
+})
+
+export const addItem = (item) => ({
+  type: CartActionTypes.ADD_ITEM,
+  payload: item
 })
