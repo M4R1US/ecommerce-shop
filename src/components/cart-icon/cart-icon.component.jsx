1f09b575aba90dc12b2a89b58c5b4bca7dec935b
M4R1US <baltr.marius@gmail.com>
2020-05-10 00:23:25 +0200

    Introduce following selectors:   * SelectCartItems -> select all cart items from state   * SelectCartItemsCount -> select total count of cart items

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import { connect } from "react-redux"
-import "./cart-icon.styles.scss"
 import { toggleCart } from "../../redux/cart/cart.actions.js"
+import { selectCartItemsCount } from "../../redux/cart/cart.selectors"
 
-const CartIcon = ( {toggleCart, itemCount} ) => (
- <div className="cart-icon" onClick={toggleCart} >
-   <ShoppingIcon className="shopping-icon" />
-   <span className="item-count"> {itemCount} </span>
- </div>
-);
+import "./cart-icon.styles.scss"
 
-const mapStateToProps = ({cart: { cartItems }}) => ({
-  itemCount: cartItems.reduce((accumulated, cItem) => {
-    return accumulated + cItem.quantity
-  }
-  ,0)
+
+const CartIcon = ( {toggleCart, itemCount} ) => {
+  return (
+    <div className="cart-icon" onClick={toggleCart}>
+      <ShoppingIcon className="shopping-icon"/>
+      <span className="item-count"> {itemCount} </span>
+    </div>
+  )
+};
+
+const mapStateToProps = (state) => ({
+  itemCount: selectCartItemsCount(state)
 })
 
 const mapDispatchToProps = (dispatch) => ({
