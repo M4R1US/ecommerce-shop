64dae1f21c475ff279ead45c47ada24d1eb9b33e
M4R1US <baltr.marius@gmail.com>
2020-05-12 23:54:49 +0200

    Introduce:  * Directory reducer to store data in rootReducer for futhure uses  * Directory selector, to select data and avoid memory overloading
    Remake Directory component to functional component since the state
    is not needed anymore.

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,13 +1,18 @@
 import  { combineReducers } from "redux"
 import { persistReducer } from "redux-persist";
+
 import storage from "redux-persist/lib/storage"
 
 // Default reducers
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
+import directoryReducer from "./directory/directory.reducer";
+
+
 const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  directory: directoryReducer
 });
 
 //Session storage configuration
